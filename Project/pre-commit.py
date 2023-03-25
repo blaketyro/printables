fcf24a6f0cf7428536a463ac71ebca059845c8fd
Blake <blaketyro@gmail.com>
2023-03-25 09:08:31 -0700

    Wrote readme generating script

---
+++ Project/pre-commit.py
@@ -0,0 +1,79 @@
+# This script prefixes the folder name to all of the included_files in every folder in this scripts directory.
+# e.g. "A4.pdf" becomes "Asymptotic_Notation_A4.pdf".
+# This way the files have descriptive names, which otherwise would have to be done manually in Affinity Designer.
+# It works even to rename the prefix if the folder name is changed (this can error if the files are named weirdly).
+#
+# This script also generates README.md since the table is tedious to create manually.
+#
+# This is used as a pre-commit hook (.git/hooks/pre-commit) but can be run separately too.
+
+import pathlib
+included_files = ['A4.pdf', 'A4.png', 'A4.svg', 'Letter.pdf', 'Letter.png',
+                  'Letter.svg', 'Poster.pdf', 'Poster.png', 'Poster.svg', 'Source.afdesign']
+excluded_folders = ['_Template_Folder']
+root = pathlib.Path(__file__).parent
+
+
+def rename_files():
+    for folder in root.iterdir():
+        if not folder.is_dir() or folder.name in excluded_folders:
+            continue
+
+        prefix = f'{folder.name}_'
+        for file in folder.iterdir():
+            if not file.is_file():
+                continue
+
+            for suffix in included_files:
+                if file.name.endswith(suffix):
+                    new_file = file.with_name(prefix + suffix)
+                    if file.name != new_file.name:
+                        print(f'Renaming {file.name} to {new_file.name}')
+                        file.rename(new_file)
+                        break
+
+
+table_header = """
+| Topic | Letter Size (8.5in&times;11in) | A4 Size (210mm&times;297mm) | Poster Size (24in&times;36in) |
+| ----- | ------------------------------ | --------------------------- | ----------------------------- |
+"""
+
+
+def raw_link(slug, size, ext):
+    return f'https://raw.githubusercontent.com/blaketyro/printables/main/Project/{slug}/{slug}_{size}.{ext}'
+
+
+def make_table_cell(folder, size, image=True):
+    slug, name = folder.name, folder.name.replace('_', ' ')
+    png = raw_link(slug, size, "png")
+    pdf = raw_link(slug, size, "pdf")
+    svg = raw_link(slug, size, "svg")
+    cell = f'[![{name}]({png})]({png}) <br> ' if image else ''
+    cell += f'**[PDF]({pdf})&nbsp;&nbsp;[PNG]({png})&nbsp;&nbsp;[SVG]({svg})**'
+    return cell
+
+
+def make_table_row(folder):
+    slug, name = folder.name, folder.name.replace('_', ' ')
+    topic = f'**{name}** <br> ([folder](/Project/{slug}))'
+    letter = make_table_cell(folder, "Letter")
+    a4 = make_table_cell(folder, "A4")
+    poster = make_table_cell(folder, "Poster", False)
+    parts = [topic, letter, a4, poster]
+    return '| ' + ' | '.join(parts) + ' |\n'
+
+
+def create_readme():
+    table = table_header
+    for folder in root.iterdir():
+        if not folder.is_dir() or folder.name in excluded_folders:
+            continue
+        table += make_table_row(folder)
+
+    text = (root / 'readme-template.md').read_text()
+    text = text.replace('{{TABLE}}', table.strip())
+    (root.parent / 'README.md').write_text(text)
+
+
+rename_files()
+create_readme()
---
+++ Project/readme-template.md
@@ -0,0 +1,58 @@
+# [Printables - github.com/blaketyro/printables](https://github.com/blaketyro/printables)
+
+A collection of free, educational, computer science posters, ready to print or link in a variety of sizes and formats. ([About the project.](#about))
+
+<!-- markdownlint-disable MD033 -->
+
+{{TABLE}}
+
+## About
+
+### License
+
+This repo and all the printables are [MIT Licensed](/LICENSE.md), meaning you can use or modify them for free as long as
+you provide attribution (a link to [github.com/blaketyro/printables](https://github.com/blaketyro/printables)
+suffices).
+
+Please, save and print them and use them in your classrooms and workplaces and for personal edification. I make
+them for fun as a hobby and that's exactly what I want to see happen!
+
+### Project Structure
+
+All the files for each printable exist in their own folder in the main [Project](/Project) directory. The printables are
+made in the [Affinity Designer](https://affinity.serif.com/en-us/designer/) vector graphics software and the original
+[afdesign](https://fileinfo.com/extension/afdesign) files can found in each printable's folder.
+
+The files are exported at three paper sizes
+
+-   Letter (8.5in×11in)
+-   A4 (210mm×297mm)
+-   Poster (24in×36in)
+
+and in three file formats:
+
+-   PDF (recommended for printing)
+-   PNG (at 200 dpi)
+-   SVG
+
+giving 9 final files for each printable.
+
+A pre-commit hook runs [pre-commit.py](/Project/pre-commit.py) to create this readme with the table above, and improve
+the names Affinity Designer gives the files.
+
+### Inspiration
+
+Computer science is my favorite field of study. There are endless topics to learn about and you get to be very exacting.
+The problem is there are _too_ many things to learn about and remember, so I started making little cheat-sheet word
+documents to help me memorize important computer science topics. Then I realized I could make better diagrams using my
+skills as a former vector graphics artist, and thought I may as well make them public while I'm at it.
+
+I wanted them to be printable (and not just webpages) since anything printable can work fine as a webpage, but not
+always vice versa.
+
+They are intentionally mostly black and white since not everyone has a color printer or can even see colors.
+It also just makes the design easier and faster.
+
+### Issues
+
+Notice a typo, bug, or error? Please [report it as an issue](https://github.com/blaketyro/printables/issues).
